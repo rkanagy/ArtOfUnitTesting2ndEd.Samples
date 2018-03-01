f0abc03c27e2c9565f2be9b54b7ce34e75cb4bd6
Ronald Kanagy <ron.kanagy@gmail.com>
2018-03-01 07:17:35 -0500

    -Implement a StandardStringParser that can extract its version from the header -Add unit tests that can parse a single-digit version, a version with minor version, and a version with revision

---
+++ MyLogAn3.UnitTests/StandardStringParserTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace MyLogAn3.UnitTests
+{
+    [TestFixture]
+    public class StandardStringParserTests
+    {
+        private StandardStringParser GetParser(string input)
+        {
+            return new StandardStringParser(input);
+        }
+
+        [Test]
+        public void GetStringVersionFromHeader_SingleDigit_Found()
+        {
+            const string input = "header;version=1;\n";
+            var parser = GetParser(input);
+
+            var versionFromHeader = parser.GetStringVersionFromHeader();
+            Assert.AreEqual("1", versionFromHeader);
+        }
+
+        [Test]
+        public void GetStringVersionFromHeader_WithMinorVersion_Found()
+        {
+            const string input = "header;version=1.1;\n";
+            var parser = GetParser(input);
+
+            var versionFromHeader = parser.GetStringVersionFromHeader();
+            Assert.AreEqual("1.1", versionFromHeader);
+        }
+
+        [Test]
+        public void GetStringVersionFromHeader_WithRevision_Found()
+        {
+            const string input = "header;version=1.1.1;\n";
+            var parser = GetParser(input);
+
+            var versionFromHeader = parser.GetStringVersionFromHeader();
+            Assert.AreEqual("1.1.1", versionFromHeader);
+        }
+    }
+}
---
+++ MyLogAn3/StandardStringParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyLogAn3
+{
+    public class StandardStringParser
+    {
+        private readonly string _input;
+
+        public StandardStringParser(string input)
+        {
+            _input = input;
+        }
+
+        public string GetStringVersionFromHeader()
+        {
+            const string versionTag = "version=";
+            const string delimiter = ";";
+
+            var versionIdx = _input.IndexOf(versionTag, StringComparison.Ordinal);
+            var delimiterIdx = _input.IndexOf(delimiter, versionIdx, StringComparison.Ordinal);
+            var versionStart = versionIdx + versionTag.Length;
+            var version = _input.Substring(versionStart, delimiterIdx - versionStart);
+
+            return version;
+        }
+    }
+}
